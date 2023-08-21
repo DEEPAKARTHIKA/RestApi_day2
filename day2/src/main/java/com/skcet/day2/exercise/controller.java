4d482d3cafc0edec7d0408f7f86b69acd270284d
DEEPAKARTHIKA <108518917+DEEPAKARTHIKA@users.noreply.github.com>
2023-08-21 09:12:12 +0530

    Add files via upload

---
+++ day2/src/main/java/com/skcet/day2/exercise/controller.java
@@ -0,0 +1,15 @@
+package com.skcet.day2.exercise;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+       @Value("${studentName}")
+       private String studentName;
+       @GetMapping("getName")
+       public String getName() {
+    	   return "Name is: "+studentName;
+       }
+}
